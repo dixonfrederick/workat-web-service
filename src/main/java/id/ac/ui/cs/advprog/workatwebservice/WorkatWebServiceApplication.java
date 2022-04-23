aea304aad5e20fadfa087c054e10169fb8fd1a56
Adrian Ardizza <adrian.ardizza@gmail.com>
2022-04-23 15:23:14 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/workatwebservice/WorkatWebServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.workatwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkatWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkatWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/workatwebservice/WorkatWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.workatwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkatWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
